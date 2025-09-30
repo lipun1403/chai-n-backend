1eed17965aeb1779c4b5a8d9c2ace393ee7cf810
lenkapradyumna <pradyumna_lenka_14.3@yahoo.com>
2025-09-30 16:07:33 +0530

    add user account management features: change password, get current user, update account details, update avatar, and update cover image

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -219,4 +219,164 @@ const refreshAccessToken = asyncHandler(async(req,res) =>{
     }
 });
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken };
+// change password
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+    const {oldPassword, newPassword} = req.body;
+
+    // validation
+    if(!oldPassword || !newPassword) {
+        throw new ApiError("Old password and new password are required", 400);
+    }
+
+    // find user
+    const user = await User.findById(req.user?._id);
+
+    // check if user exists
+    if(!user) {
+        throw new ApiError("User not found", 404);
+    }
+
+    // check for old password
+    const isOldPasswordValid = await user.isPasswordCorrect(oldPassword);
+
+    if(!isOldPasswordValid) {
+        throw new ApiError("Invalid old password", 401);
+    }
+
+    // update password
+    user.password = newPassword;
+    await user.save({validateBeforeSave: false});
+
+    // successful
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            {},
+            "Password changed successfully"
+        )
+    );
+});
+
+// get current user
+const getCurrentUser = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user?._id);
+    if (!user) {
+        throw new ApiError("User not found", 404);
+    }
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            { user },
+            "Current user fetched successfully"
+        )
+    );
+})
+
+// update account details
+const updateAccountDetails = asyncHandler(async (req, res) => {
+    const { fullName, email } = req.body;
+    if(!fullName || !email) {
+        throw new ApiError(400, "All fields are required");
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        { 
+            $set: {
+                fullName: fullName,
+                email: email
+            }
+        },
+        { new: true }
+    )
+    .select("-password");
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200, 
+            { user }, 
+            "Account details updated successfully"
+        )
+    );
+})
+
+// update avatar 
+const updateAvatar = asyncHandler(async (req,res) => {
+    const avatarLocalPath = req.file?.path;
+
+    if(!avatarLocalPath) {
+        throw new ApiError(400,"Avatar file is missing");
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+    if(!avatar.url) {
+        throw new ApiError(400,"Error while uploading on avatar");
+    }
+
+    // update user avatar
+    const user =await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                avatar: avatar.url
+            }
+        },
+        {new: true}
+    ).select("-password");
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user,
+            "Avatar updated successfully"
+        )
+    )
+});
+
+// update user cover image
+const updateUserCoverImage = asyncHandler(async (req,res) => {
+    const coverImageLocalPath = req.file?.path;
+
+    if(!coverImageLocalPath) {
+        throw new ApiError(400,"Cover image file is missing");
+    }
+
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    if(!coverImage.url) {
+        throw new ApiError(400,"Error while uploading cover image");
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                coverImage: coverImage.url
+            }
+        },
+        {new: true}
+    ).select("-password");
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user,
+            "Cover image updated successfully"
+        )
+    );
+});
+
+
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateAvatar, updateUserCoverImage };
